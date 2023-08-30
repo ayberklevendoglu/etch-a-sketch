70f8884703ab3b1ca4b0c5428d259335fecf64c6
Ayberk Levendoglu <ayberklevendoglu@gmail.com>
2023-08-30 12:47:10 +0300

    finished the buttons and added some css

--- script.js
+++ script.js
@@ -1,3 +1,5 @@
+let color = "black";
+
 let board = document.querySelector(".board");
 let mouseDown = false;
 board.onmousedown = function (event) {
@@ -21,31 +23,40 @@ function createBoard(size) {
   for (let i = 0; i < size * size; i++) {
     const div = document.createElement("div");
     div.classList.add("cell");
-    div.addEventListener("mouseenter", () => {
-      console.log(mouseDown);
-      if (mouseDown) {
-        blackBoard(div);
-      }
-    });
+    div.addEventListener("mouseenter", colorSquare);
     board.appendChild(div);
   }
 }
 
 function changeSize(input) {
-  createBoard(input);
+  if (input > 15 && input <= 100) {
+    createBoard(input);
+  } else {
+    console.log("ERROR");
+  }
 }
-
 
 function resetBoard() {
   let cells = document.querySelectorAll(".cell");
   cells.forEach((cell) => {
-    cell.classList.remove("dark-bg");
+    cell.style.backgroundColor = "";
   });
 }
 
 let resetBtn = document.querySelector(".reset-btn");
 resetBtn.addEventListener("click", resetBoard);
 
-function blackBoard(cell) {
-  cell.classList.add("dark-bg");
+function colorSquare(){
+  if(mouseDown){
+    if(color === 'random'){
+      this.style.backgroundColor = 'hsl(' + (360 * Math.random()) + ',50%,50%)';
+    }else{
+      this.style.backgroundColor = color;
+    }
 }
+}
+
+function changeColor(choice){
+  color = choice;
+}
+
